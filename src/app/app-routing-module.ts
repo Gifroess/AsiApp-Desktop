f9ecc1308f10e30d9f19e6cf2d05b5eddff0858b
Giovana <gigifroesesilva@gmail.com>
2026-08-23 02:09:05 -0300

    organizando pastas/ login component criado

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './components/login/login';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: Login},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
