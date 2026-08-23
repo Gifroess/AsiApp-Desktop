de3ec36bf9d218b1bbb78ea3b91b4726f178b77f
Giovana <gigifroesesilva@gmail.com>
2026-08-23 02:30:45 -0300

    integrando Firebase ao projeto

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Login } from './components/login/login';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../assets/environments/environment';
 @NgModule({
   declarations: [App, Login],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+  ],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
---
+++ src/assets/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyBMwDdJZm7NWpd-jMuvwKcdas5gfIRXXQ4",
+        authDomain: "asiapp-994a7.firebaseapp.com",
+        projectId: "asiapp-994a7",
+        storageBucket: "asiapp-994a7.firebasestorage.app",
+        messagingSenderId: "774688698032",
+        appId: "1:774688698032:web:2beb65af780cf5984cd575",
+        measurementId: "G-V1LSTJ402D"
+    }
+}
