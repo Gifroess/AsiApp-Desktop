adea3a1c0a435d7de798b08d0ef62f9c0ab51b24
miguelcortez1105 <miguelcortezcavalcante2005@gmail.com>
2026-09-01 18:27:17 -0300

    Erro de carregamento de imagens necessarias da tela de login e cadastro

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -14,9 +14,11 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { environment } from '../assets/environments/environment';
 import { Cadastro } from './components/cadastro/cadastro';
+import { Perfil } from './components/perfil/perfil';
+import { RecuperarSenha } from './components/recuperar-senha/recuperar-senha';
 
 @NgModule({
-  declarations: [App, Login, Cadastro],
+  declarations: [App, Login, Cadastro, GestaoProjetos, Perfil, Sidebar, RecuperarSenha],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
@@ -24,6 +26,7 @@ import { Cadastro } from './components/cadastro/cadastro';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    AngularFireStorageModule,
   ],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
