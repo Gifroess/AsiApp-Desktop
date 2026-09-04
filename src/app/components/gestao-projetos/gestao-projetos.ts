c49587480cf53b5c58c5b5b092cbc507d1ca3957
Guilherme Sampaio <pcoelhobr@gmail.com>
2026-09-03 22:31:09 -0300

    feat: atualiza estrutura de arquivos e adiciona novos componentes na tela de gestão de projetos

--- src/app/components/gestao-projetos/gestao-projetos.ts
+++ src/app/components/gestao-projetos/gestao-projetos.ts
@@ -1,9 +1,56 @@
 import { Component } from '@angular/core';
 
+interface Projeto {
+  nome: string;
+  gerente: string;
+  area: string;
+  prazo: string;
+  valor: string;
+  status: string;
+  cor: string;
+  membros: string[];
+  descricao: string;
+  aberto: boolean;
+}
+
 @Component({
   selector: 'app-gestao-projetos',
   standalone: false,
   styleUrl: './gestao-projetos.scss',
   templateUrl: './gestao-projetos.html',
 })
-export class GestaoProjetos {}
+export class GestaoProjetos {
+  //projetos cadastrados
+  projetos: Projeto[] = [
+    {
+      nome: 'Projeto Asimov',
+      gerente: 'Lucas',
+      area: 'Marketing',
+      prazo: '30/09/2026',
+      valor: 'R$99.999,99',
+      status: 'Em andamento',
+      cor: 'bg-yellow-400',
+      membros: ['Lucas', 'Ana', 'Carlos'],
+      descricao: 'Projeto voltado para criação e evolução do aplicativo Asimov.',
+      aberto: false,
+    },
+
+    {
+      nome: 'Aplicativo Mobile',
+      gerente: 'Giovanna',
+      area: 'Desenvolvimento',
+      prazo: '15/10/2026',
+      valor: 'R$50.000,00',
+      status: 'A iniciar',
+      cor: 'bg-blue-500',
+      membros: ['Giovanna', 'Pedro'],
+      descricao: 'Desenvolvimento da aplicação mobile integrada ao sistema.',
+      aberto: false,
+    },
+  ];
+
+  //abre ou fecha detalhes do projeto
+  abrirProjeto(projeto: Projeto) {
+    projeto.aberto = !projeto.aberto;
+  }
+}
